7e78211e467a0cc8365fa252b1c82888d22283b2
nopachyn <nova1784@gmail.com>
2025-11-07 22:16:18 +0700

    Menambahkan index.php awal di branch main

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+</head>
+<body>
+    <h1>Selamat Datang di Aplikasi CRUD</h1>
+    <p>Ini adalah halaman utama sebelum melakukan merge branch lain.</p>
+
+    <ul>
+        <li><a href="#">Koneksi Database</a></li>
+        <li><a href="#">Tambah Data</a></li>
+    </ul>
+</body>
+</html>
